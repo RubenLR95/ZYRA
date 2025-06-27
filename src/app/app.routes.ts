e7285676699c67693f2117c35d6661b667776dc6
RubénLR95 <ruben.lopez.ras@gmail.com>
2025-06-27 13:51:06 +0200

    Created y added component routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ChatComponent } from './features/chat/chat.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [       //AQUI VAN TODAS LAS RUTAS. SE CONECTAN UNOS COMPONENTES CON OTROS.
+    // RUTA raiz QUE REDIRIGE DIRECTAMENTE A chat
+    {path: '', redirectTo: 'chat', pathMatch: 'full'},
+    //RUTA CHAT QUE RENDERIZA DIRECTAMENTE CHATCOMPONENT
+    {path: 'chat', component: ChatComponent},
+    //RUTA COMODÍN QUE REDIRIGE CUALQUIER OTRA RUTA A 'chat'
+    { path : '**', redirectTo: 'chat'}
+];
