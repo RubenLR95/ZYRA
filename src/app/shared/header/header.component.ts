e51b4f7856167e75b4571a060452ac713fa75a93
RubénLR95 <ruben.lopez.ras@gmail.com>
2025-06-27 16:05:09 +0200

    On set up header component

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewEncapsulation } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+import {RippleModule} from 'primeng/ripple';
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    ToolbarModule,
+    ButtonModule,
+    RippleModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: [ './header.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HeaderComponent {
+    //BANDERA QUE INDICA EL MODO OSCURO (TRUE) O CLARO (FALSE)
+    darkMode = false;
 
+    toggleTheme(){   //ESTA FUNCIÓN ALTERNA EL TEMA DE LA APLICACION DE CLARO A OSCURO
+      this.darkMode = !this.darkMode;
+      document.body.classList.toggle('dark', this.darkMode); 
+    }
 }
